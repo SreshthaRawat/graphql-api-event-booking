d23640cd2466f3c9d0b01bad017f7171d14b8ad8
Sreshtha Rawat <sreshtha118@gmail.com>
2026-02-23 12:26:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+const isAuthMiddleware = require('./middleware/is-auth');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(isAuthMiddleware);
+
+const port = process.env.PORT || 3000;
+const graphQlSchema = require('./graphql/schemas/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+app.use('/graphql', graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+}));
+
+const mongoConnectionUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-rjk9d.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoConnectionUrl, { useNewUrlParser: true })
+    .then(() => {
+        app.listen(port, () => { console.log(`Server started on port ${port}`) });
+    })
+    .catch(err => {
+        console.error(err);
+    });
